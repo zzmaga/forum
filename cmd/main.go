84251f9329a8c2edfebad70c8e36b8dc214ea1e9
mtastan <magjantastanov@gmail.com>
2025-05-21 23:22:04 +0500

    req errors

--- cmd/main.go
+++ cmd/main.go
@@ -8,6 +8,15 @@ import (
 // Define a home handler function which writes a byte slice containing
 // "Hello from MAC" as the response body
 func home(w http.ResponseWriter, r *http.Request) {
+	// Check if the current request URL path exactly matches "/".
+	// If it doesn't, use
+	// the http.NotFound() function to send a 404 response to the client.
+	// Importantly, we then return from the handler. If we don't return the handler
+	// would keep executing and also write the "Hello from MAC" message.
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
 	w.Write([]byte("Hello from MAC"))
 }
 
@@ -18,6 +27,16 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
 
 // Add a snippetCreate handler function.
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	// Use r.Method to check whether the request is using POST or not.
+	if r.Method != "POST" {
+		// If it's not, use w.WriteHeader() method to send a 405 status
+		// code and the w.Write() method to write a "Method Not Allowed"
+		// response body. We then return from the function so that the
+		// subsequent code is not executed.
+		w.WriteHeader(405)
+		w.Write([]byte("Method Not Allowed"))
+		return
+	}
 	w.Write([]byte("Create a new snippet"))
 }
 
