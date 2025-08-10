1d89b222e3cdbed9665b83a7a9f9939974eca054
mtastan <magjantastanov@gmail.com>
2025-08-11 00:05:59 +0500

    create tables

--- internal/database/db.go
+++ internal/database/db.go
@@ -22,7 +22,11 @@ func InitDB(filepath string) {
 
 	createUsersTable()
 	createSessionsTable()
-	log.Println("Database initialized successfully")
+	createCategoriesTable()
+	createPostsTable()
+	createPostCategoriesTable()
+	createCommentsTable()
+	createLikesTable()
 }
 
 func createUsersTable() {
@@ -33,7 +37,6 @@ func createUsersTable() {
 		email TEXT NOT NULL UNIQUE,
 		password TEXT NOT NULL
 	);`
-
 	_, err := DB.Exec(query)
 	if err != nil {
 		log.Fatal("Error creating users table:", err)
@@ -51,5 +54,83 @@ func createSessionsTable() {
 	_, err := DB.Exec(query)
 	if err != nil {
 		log.Fatal("Error creating sessions table:", err)
+	}
+}
+
+func createCategoriesTable() {
+	query := `
+	CREATE TABLE IF NOT EXISTS categories (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		name TEXT UNIQUE NOT NULL
+	);`
+	_, err := DB.Exec(query)
+	if err != nil {
+		log.Fatal("Error creating categories table:", err)
+	}
+}
+
+func createPostsTable() {
+	query := `
+	CREATE TABLE IF NOT EXISTS posts (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		user_id INTEGER NOT NULL,
+		title TEXT NOT NULL,
+		content TEXT NOT NULL,
+		created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+		FOREIGN KEY(user_id) REFERENCES users(id)
+	);`
+	_, err := DB.Exec(query)
+	if err != nil {
+		log.Fatal("Error creating posts table:", err)
+	}
+}
+
+func createPostCategoriesTable() {
+	query := `
+	CREATE TABLE IF NOT EXISTS post_categories (
+		post_id INTEGER NOT NULL,
+		category_id INTEGER NOT NULL,
+		PRIMARY KEY (post_id, category_id),
+		FOREIGN KEY(post_id) REFERENCES posts(id),
+		FOREIGN KEY(category_id) REFERENCES categories(id)
+	);`
+	_, err := DB.Exec(query)
+	if err != nil {
+		log.Fatal("Error creating post_categories table:", err)
+	}
+}
+
+func createCommentsTable() {
+	query := `
+	CREATE TABLE IF NOT EXISTS comments (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		post_id INTEGER NOT NULL,
+		user_id INTEGER NOT NULL,
+		content TEXT NOT NULL,
+		created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+		FOREIGN KEY(post_id) REFERENCES posts(id),
+		FOREIGN KEY(user_id) REFERENCES users(id)
+	);`
+	_, err := DB.Exec(query)
+	if err != nil {
+		log.Fatal("Error creating comments table:", err)
+	}
+}
+
+func createLikesTable() {
+	query := `
+	CREATE TABLE IF NOT EXISTS likes (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		user_id INTEGER NOT NULL,
+		post_id INTEGER,
+		comment_id INTEGER,
+		is_like BOOLEAN NOT NULL,
+		FOREIGN KEY(user_id) REFERENCES users(id),
+		FOREIGN KEY(post_id) REFERENCES posts(id),
+		FOREIGN KEY(comment_id) REFERENCES comments(id)
+	);`
+	_, err := DB.Exec(query)
+	if err != nil {
+		log.Fatal("Error creating likes table:", err)
 	}
 }
